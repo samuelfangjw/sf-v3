17fd2f7a9216a10dfa76c575c4305c6b36b6bd44
Samuel Fang <samuelfangjw@gmail.com>
2022-09-18 00:08:14 +0800

    feat(footer): update footer contents

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,12 @@
+import { Box, Divider, Text } from "@chakra-ui/react";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <Box textAlign="center" m="8">
+      <Divider />
+      <Text mt="4" fontSize="sm">Designed & Built by Samuel Fang, 2022</Text>
+    </Box>
+  );
 };
 
 export default Footer;
