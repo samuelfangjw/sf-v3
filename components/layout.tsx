16be8f10b44d50d72a92a30776c51ed1e456a751
Samuel Fang <samuelfangjw@gmail.com>
2022-09-17 20:47:36 +0800

    Add skeleton code

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+import Navbar from './navbar'
+import Footer from './footer'
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
