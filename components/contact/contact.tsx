56347bc87d970377255270fca28a4cdf3259d910
Samuel Fang <samuelfangjw@gmail.com>
2022-09-18 22:59:31 +0800

    feat(contact): add contact section

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,7 +1,42 @@
 import Section from "../section";
+import { Link, List, ListIcon, ListItem, Text } from "@chakra-ui/react";
+import { FiMail } from "react-icons/fi";
+import constants from "../../utils/constants";
+import { FaGithub } from "react-icons/fa";
 
 const Contact = () => {
-  return <Section id="contact" title="Contact">Placeholder</Section>;
+  return (
+    <Section id="contact" title="Get In Touch">
+      <Text>
+        I&apos;m always open to new opportunities, feel free to drop me a
+        message if you have any questions or just want to say hi!
+      </Text>
+      <List>
+        <ListItem>
+          <ListIcon as={FiMail} />
+          <Text display="inline" fontWeight="bold">
+            Email:
+          </Text>
+          <Link
+            href={`mailto:${constants.email.value}`}
+            color="orange.300"
+            isExternal
+          >
+            {constants.email.display}
+          </Link>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={FaGithub} />
+          <Text display="inline" fontWeight="bold">
+            GitHub:
+          </Text>
+          <Link href={constants.github.value} color="orange.300" isExternal>
+            {constants.github.display}
+          </Link>
+        </ListItem>
+      </List>
+    </Section>
+  );
 };
 
 export default Contact;
---
+++ utils/constants.ts
@@ -0,0 +1,12 @@
+const constants = {
+  email: {
+    display: "samuelfangjw@gmail.com",
+    value: "samuelfangjw@gmail.com"
+  },
+  github: {
+    display: "@samuelfangjw",
+    value: "https://github.com/samuelfangjw"
+  }
+};
+
+export default constants;
