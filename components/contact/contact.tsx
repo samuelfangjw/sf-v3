6abb12a297110aecefc2999ef9b17016c1b1dc86
Samuel Fang <samuelfangjw@gmail.com>
2022-09-20 21:52:00 +0800

    feat(contact): add contact details

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,19 +1,26 @@
 import Section from "../section";
-import { Link, List, ListIcon, ListItem, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { FiMail } from "react-icons/fi";
 import constants from "../../utils/constants";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
-  const color = useColorModeValue("orange.400", "orange.300")
+  const color = useColorModeValue("orange.400", "orange.300");
 
   return (
     <Section id="contact" title="Get In Touch">
-      <Text>
+      <Text marginBottom={8}>
         I&apos;m always open to new opportunities, feel free to drop me a
         message if you have any questions or just want to say hi!
       </Text>
-      <List>
+      <List spacing={2}>
         <ListItem>
           <ListIcon as={FiMail} />
           <Text display="inline" fontWeight="bold">
@@ -22,6 +29,7 @@ const Contact = () => {
           <Link
             href={`mailto:${constants.email.value}`}
             color={color}
+            marginLeft="36px"
             isExternal
           >
             {constants.email.display}
@@ -32,8 +40,17 @@ const Contact = () => {
           <Text display="inline" fontWeight="bold">
             GitHub:
           </Text>
-          <Link href={constants.github.value} color={color} isExternal>
+          <Link href={constants.github.value} color={color} marginLeft="22px" isExternal>
             {constants.github.display}
+          </Link>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={FaLinkedin} />
+          <Text display="inline" fontWeight="bold">
+            LinkedIn:
+          </Text>
+          <Link href={constants.linkedin.value} color={color} marginLeft="10px" isExternal>
+            {constants.linkedin.display}
           </Link>
         </ListItem>
       </List>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,16 @@
-const constants = {
+const constants: { [id: string]: { display: string; value: string } } = {
   email: {
     display: "samuelfangjw@gmail.com",
-    value: "samuelfangjw@gmail.com"
+    value: "samuelfangjw@gmail.com",
   },
   github: {
     display: "@samuelfangjw",
-    value: "https://github.com/samuelfangjw"
-  }
+    value: "https://github.com/samuelfangjw",
+  },
+  linkedin: {
+    display: "@samuelfangjw",
+    value: "https://www.linkedin.com/in/samuelfangjw/",
+  },
 };
 
 export default constants;
