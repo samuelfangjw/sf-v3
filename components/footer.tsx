aaa1061aca00239e42d24790660c5fbf0648ab73
Samuel Fang <samuelfangjw@gmail.com>
2022-09-23 19:32:02 +0800

    feat(footer): add back to top button

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,33 @@
-import { Box, Divider, Text } from "@chakra-ui/react";
+import { ArrowUpIcon } from "@chakra-ui/icons";
+import { Box, Button, Divider, Text } from "@chakra-ui/react";
 import TextLink from "./text-link";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <Box textAlign="center" m="8">
+    <Box textAlign="center" position="relative" m="8">
       <Divider />
       <Box mt="4" fontSize="sm">
         <TextLink link="https://github.com/samuelfangjw/sf-v3">
           Designed & Built
         </TextLink>
         <Text display="inline"> by Samuel Fang, 2022</Text>
+      </Box>
+      <Box
+        display={{ base: "none", sm: "block" }}
+        position="absolute"
+        right="0"
+        top="0"
+        my="1.5"
+      >
+        <Button variant="ghost" onClick={scrollToTop}>
+          <ArrowUpIcon />
+        </Button>
       </Box>
     </Box>
   );
